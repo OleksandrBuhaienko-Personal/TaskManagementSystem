65a5b7be51f479fa453aaa7d658d3d0c47de22dd
OleksandrBuhaienko <oleksandr.buhaienko@tech-stack.io>
2025-09-17 20:33:36 +0300

    [TMS] Add Persistence project

--- src/TaskManagementSystem.WebAPI/Program.cs
+++ src/TaskManagementSystem.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
 namespace TaskManagementSystem.WebAPI;
 
 public class Program
@@ -6,20 +9,21 @@ public class Program
   {
     var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
-
+    
+    
     builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(c =>
+    {
+      c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    });
 
     var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
+    
     if (app.Environment.IsDevelopment())
     {
       app.UseSwagger();
-      app.UseSwaggerUI();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
     }
 
     app.UseHttpsRedirection();
