4760fc1e8d4706cc437181da772507a15e7d0fa4
OleksandrBuhaienko <oleksandr.buhaienko@tech-stack.io>
2025-09-18 16:47:58 +0300

    [TMS] Add Application layer

---
+++ src/Application/TaskManagementSystem.Application/AppModule.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TaskManagementSystem.Application;
+
+public static class AppModule
+{
+  public static void ConfigureApplicationModules(this IServiceCollection services, IConfiguration configuration)
+  {
+  }
+}
